df96b9878ccf0eac361a8b4c01108e90b8a9c0ce
Sanyam Arora <iamsanyamarora@gmail.com>
2022-10-16 01:01:15 +0530

    IO_TEST

---
+++ LPC2378/IO_TEST/main.c
@@ -0,0 +1,26 @@
+#include <LPC23xx.h>
+#include <stdio.h>
+
+void delay(void);
+
+int main(){
+PINSEL0=0x00;
+IODIR0=0x000000FF;
+	while(1){
+if((IOPIN0 & 0x00000100)==0x00000100){
+IOSET0=0x000000FF;
+
+}	
+IOCLR0=0x000000FF;
+}
+
+}
+
+
+void delay(){
+for(int i=0;i<2;i++){
+}
+
+
+
+}
