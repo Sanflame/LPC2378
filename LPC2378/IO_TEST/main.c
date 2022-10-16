83241f8dbe94a2479f275d4cb92becc0500ae545
Sanyam Arora <iamsanyamarora@gmail.com>
2022-10-16 21:08:09 +0530

    IO_TEST

--- LPC2378/IO_TEST/main.c
+++ LPC2378/IO_TEST/main.c
@@ -1,18 +1,18 @@
 #include <LPC23xx.h>
 #include <stdio.h>
 
-void delay(void);
+void delay(void);															// TIMER IS NOT USED , configure DELAY as per requirement
 
-int main(){
-PINSEL0=0x00;
-IODIR0=0x000000FF;
+int main(){																		//MAIN FUNC
+PINSEL0=0x00;																	//SELECT PORT0 as GPIO
+IODIR0=0x000000FF;														//SET PORT0 0-7 as output, rest input
 	while(1){
-if(IOPIN0<<9){
-IOSET0=0x000000FF;
+if(IOPIN0 & (1<<8)){													//CHECK 8th bit of PORT0 (if 8th bit == 1 ) then condition is true
+IOSET0=0x000000FF;														// set 0-7 pin of port0 as high
 
 }	
 else{
-IOCLR0=0x000000FF;
+IOCLR0=0x000000FF;														//clear pin 0-7 of port 0
 }
 	}
 
